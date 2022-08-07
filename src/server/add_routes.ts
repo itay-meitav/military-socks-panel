fcf9a6c01ea467524e30fe98fefc8c2f9d2d423e
itay-meitav <itai9999@gmail.com>
2022-08-07 14:49:37 +0300

    editing/adding/deleting almost work

---
+++ src/server/add_routes.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import { } from '"DB"'
+
+const addRouter = express.Router()
+addRouter.get(
+    ["/sock"],
+    (req, res) => {
+        res.render("add", { info: {} });
+    }
+);
+
+
+addRouter.get(
+    "/officer",
+    (req, res) => {
+        res.render("add", { info: {} });
+    }
+);
+
+addRouter.get(
+    "/history",
+    (req, res) => {
+        res.render("add", { info: {} });
+    }
+);
+
+addRouter.get(
+    "/location",
+    (req, res) => {
+        res.render("add", { info: {} });
+    }
+);
+
+
+
+
+export default addRouter
