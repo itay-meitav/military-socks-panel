660f6f86a298edf979d67199431182aef744925a
itay-meitav <itai9999@gmail.com>
2022-08-10 15:16:11 +0300

    updated deploy steps

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
+#! /bin/bash
+
+
+HEROKU_APP_NAME="cyber4s-socks"
+
 cd deploy
 git init
 echo "\n .env" >> "./.gitignore"
-heroku git:remote -a cyber4s-socks
+heroku git:remote -a "$HEROKU_APP_NAME"
 git add -A
 git commit -m "deploy"
 git push heroku master -f
