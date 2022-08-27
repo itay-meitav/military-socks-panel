cb87c3f3ea63726fe61bb889af7c129dcb31065c
itay-meitav <itai9999@gmail.com>
2022-08-28 01:06:03 +0300

    fixed and added search functionality

--- client/src/components/mini/SearchForm.tsx
+++ client/src/components/mini/SearchForm.tsx
@@ -1,108 +1,90 @@
 import React, { useEffect } from "react";
-import {
-	Autocomplete,
-	Backdrop,
-	Box,
-	Button,
-	Fade,
-	Modal,
-	TextField,
-	Typography,
-	InputAdornment,
-} from "@mui/material";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { Clear } from "@mui/icons-material/";
+
+type TOption = {
+  label: string;
+  id: string | number;
+};
 
 type IProps = {
-	options: {
-		label: string;
-		id: string | number;
-	}[];
-	text: string;
-	setText: Function;
-	onSubmit: (val?: string) => void;
-	closeSearch: Function;
+  options: TOption[];
+  text: string;
+  onSubmit: (val?: string) => void;
 };
 
 function SearchForm(props: IProps) {
-	const [showOptions, setShowOptions] = React.useState(false);
-	const [inputText, setInputText] = React.useState(props.text);
-	const form = React.useRef<HTMLFormElement>(null);
-	const input = React.useRef<HTMLInputElement>(null);
-	useEffect(() => {
-		input.current?.focus();
-		console.log(input.current);
-	}, []);
+  const [showOptions, setShowOptions] = React.useState(false);
+  const [inputText, setInputText] = React.useState(props.text);
+  const form = React.useRef<HTMLFormElement>(null);
+  const input = React.useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    input.current?.focus();
+    console.log(input.current);
+  }, []);
 
-	return (
-		<form
-			ref={form}
-			autoComplete={"off"}
-			onSubmit={(e) => {
-				e.preventDefault();
-				if (props.text == inputText) props.closeSearch();
-				props.setText(inputText);
-			}}
-			className="search-form"
-			tabIndex={0}
-			onBlur={(e) => {
-				if (!form.current?.contains(e.relatedTarget)) setShowOptions(false);
-			}}
-			onKeyDown={(e) => {
-				// console.log(e.key);
-				if (e.key === "ArrowUp") {
-				}
-				if (e.key === "ArrowDown") {
-				}
-			}}
-		>
-			<TextField
-				style={{ width: "100%" }}
-				inputProps={{
-					onInput: (e) => setInputText(e.currentTarget.value),
-					onFocusCapture: () => setShowOptions(true),
-					ref: input,
-					autoFocus: true,
-				}}
-				InputProps={{
-					endAdornment: (
-						<InputAdornment position="end">
-							<ArrowDropDownIcon></ArrowDropDownIcon>
-						</InputAdornment>
-					),
-				}}
-				label="Search"
-				value={inputText}
-			/>
-			{inputText.length ? (
-				<div className="options-container">
-					<div className="options">
-						{props.options
-							.filter((option) =>
-								option.label
-									.toLowerCase()
-									.includes(inputText.toLowerCase())
-							)
-							.map((option) => (
-								<button
-									type={"button"}
-									key={option.id}
-									className="option"
-									onClick={() => {
-										console.log("option clicked");
-										props.setText(option.label);
-										if (props.text == inputText) props.closeSearch();
-									}}
-								>
-									{option.label}
-								</button>
-							))}
-					</div>
-				</div>
-			) : (
-				<></>
-			)}
-		</form>
-	);
+  return (
+    <form
+      ref={form}
+      autoComplete={"off"}
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.onSubmit(inputText);
+      }}
+      className="search-form"
+      onBlur={(e) => {
+        if (!form.current?.contains(e.relatedTarget)) setShowOptions(false);
+      }}
+    >
+      <TextField
+        style={{ width: "100%" }}
+        inputProps={{
+          onInput: (e) => setInputText(e.currentTarget.value),
+          onFocusCapture: () => setShowOptions(true),
+          ref: input,
+          autoFocus: true,
+        }}
+        InputProps={{
+          endAdornment: inputText ? (
+            <InputAdornment position="end">
+              <IconButton onClick={() => props.onSubmit("")}>
+                <Clear></Clear>
+              </IconButton>
+            </InputAdornment>
+          ) : (
+            <></>
+          ),
+        }}
+        label="Search"
+        value={inputText}
+      />
+      {inputText.length || showOptions ? (
+        <div className="options-container">
+          <div className="options">
+            {props.options
+              .filter((option) =>
+                option.label.toLowerCase().includes(inputText.toLowerCase())
+              )
+              .map((option) => (
+                <button
+                  type={"button"}
+                  key={option.id}
+                  className="option"
+                  onClick={() => {
+                    console.log("option clicked");
+                    props.onSubmit(option.label);
+                  }}
+                >
+                  {option.label}
+                </button>
+              ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </form>
+  );
 }
 
 export default SearchForm;
--- client/src/components/mini/SearchPopup.tsx
+++ client/src/components/mini/SearchPopup.tsx
@@ -1,148 +1,107 @@
-import {
-	Autocomplete,
-	Backdrop,
-	Box,
-	Button,
-	Fade,
-	Modal,
-	TextField,
-	Typography,
-} from "@mui/material";
+import { Backdrop, Box, Fade, Modal } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { PageContext } from "../../App";
 import config from "../../assets/config";
-import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 import SearchForm from "./SearchForm";
 import { getUniq } from "../../assets/functions";
 const style = {
-	position: "absolute" as "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	bgcolor: "rgb(245, 245, 245)",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 4,
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "rgb(245, 245, 245)",
+  border: "1px #000",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "8px",
 };
 
 type IProps = {
-	open: boolean;
-	setOpen: Function;
+  open: boolean;
+  setOpen: Function;
 };
 
 type IInputOptions = {
-	id: number;
-	label: string;
+  id: number;
+  label: string;
 };
 const validPages = ["socks", "locations", "officers"];
 function getOptions(page: string) {
-	if (!validPages.includes(page))
-		return Promise.reject("cant search for this page");
+  if (!validPages.includes(page))
+    return Promise.reject("cant search for this page");
 
-	return fetch(config.apiHost + "/api/get/search-options/" + page)
-		.then((res) => {
-			if (res.ok) return res.json();
-			else throw new Error("something went wrong");
-		})
-		.then((data) => {
-			return data;
-		});
+  return fetch(config.apiHost + "/api/get/search-options/" + page)
+    .then((res) => {
+      if (res.ok) return res.json();
+      else throw new Error("something went wrong");
+    })
+    .then((data) => {
+      return data;
+    });
 }
 
 function SearchPopup(props: IProps) {
-	const [open, setOpen] = [props.open, props.setOpen];
-	const [options, setOptions] = useState<IInputOptions[]>([]);
-	const [page] = useContext(PageContext);
-	const [retry, setRetry] = useState(0);
-	const [reload, setReload] = useState(true);
-	const location = useLocation();
-	const navigate = useNavigate();
-	const [searchParams, setSearchParams] = useSearchParams();
+  const [open, setOpen] = [props.open, props.setOpen];
+  const [options, setOptions] = useState<IInputOptions[]>([]);
+  const [page] = useContext(PageContext);
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [inputText, setInputText] = useState(searchParams.get("search") || "");
 
-	const [inputText, setInputText] = useState(searchParams.get("search") || "");
-	const [firstTime, setFirstTime] = useState(true);
+  const handleClose = () => setOpen(false);
 
-	// const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
-	useEffect(() => {
-		if (validPages.includes(page))
-			getOptions(page)
-				.then((data) => {
-					setOptions(data);
-				})
-				.catch((err) => {
-					console.log("error while fetching search options");
-					if (retry >= 0 && retry < 3) {
-						console.log("retrying to ");
+  useEffect(() => {
+    if (validPages.includes(page)) {
+      setInputText("");
+      getOptions(page)
+        .then((data) => {
+          setOptions(data);
+        })
+        .catch((err) => {
+          console.error("error while fetching search options");
+        });
+    }
+  }, [location, page]);
 
-						setRetry((pre) => pre + 1);
-						setReload((pre) => !pre);
-					} else {
-						setRetry(0);
-					}
-				});
-	}, [location, page, reload]);
+  const submitSearch = (val: string | undefined = undefined) => {
+    handleClose();
+    // remove all current search params
+    const keys = searchParams.keys();
+    let next = keys.next();
+    while (next?.value) {
+      searchParams.delete(next.value);
+      next = keys.next();
+    }
+    const value = val ?? inputText;
+    if (value) searchParams.set("search", value);
+    setSearchParams(searchParams);
+  };
 
-	useEffect(() => {
-		if (!firstTime) {
-			console.log("submitting search: ", inputText);
-
-			handleClose();
-
-			// remove all current search params
-			const keys = searchParams.keys();
-			let next = keys.next();
-			while (next?.value) {
-				searchParams.delete(next.value);
-				next = keys.next();
-			}
-			searchParams.set("search", inputText);
-			setSearchParams(searchParams);
-		} else {
-			setFirstTime(false);
-		}
-	}, [inputText]);
-
-	return (
-		<Modal
-			aria-labelledby="transition-modal-title"
-			aria-describedby="transition-modal-description"
-			open={open}
-			onClose={handleClose}
-			closeAfterTransition
-			BackdropComponent={Backdrop}
-			BackdropProps={{
-				timeout: 500,
-			}}
-		>
-			<Fade in={open}>
-				<Box sx={{ ...style, borderRadius: "15px" }}>
-					<SearchForm
-						text={inputText}
-						setText={setInputText}
-						closeSearch={handleClose}
-						onSubmit={(val: string = "") => {
-							console.log("submitting search: ", val || inputText);
-
-							handleClose();
-
-							// remove all current search params
-							const keys = searchParams.keys();
-							let next = keys.next();
-							while (next?.value) {
-								searchParams.delete(next.value);
-								next = keys.next();
-							}
-							searchParams.set("search", val || inputText);
-							setSearchParams(searchParams);
-						}}
-						options={getUniq(options, (cell) => cell.label)}
-					/>
-				</Box>
-			</Fade>
-		</Modal>
-	);
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <Box sx={style}>
+          <SearchForm
+            text={inputText}
+            onSubmit={submitSearch}
+            options={getUniq(options, (cell) => cell.label)}
+          />
+        </Box>
+      </Fade>
+    </Modal>
+  );
 }
 
 export default SearchPopup;
